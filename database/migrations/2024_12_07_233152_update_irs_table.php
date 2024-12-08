aa330adb8a6b57d3b489be35e62b7bafaf4e371c
edfaruq <14.alfaruq@gmail.com>
2024-12-08 08:50:01 +0700

    ngubah dikit tadi error di migrate

--- database/migrations/2024_12_07_233152_update_irs_table.php
+++ database/migrations/2024_12_07_233152_update_irs_table.php
@@ -6,34 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up(): void
+    /**
+     * Run the migrations.
+     */
+    public function up()
     {
         Schema::table('irs', function (Blueprint $table) {
-            // Ubah enum status
             $table->enum('status', [
                 'Belum Disetujui',
                 'Menunggu Persetujuan',
                 'Disetujui',
                 'Ditolak'
             ])->default('Belum Disetujui')->change();
-
-            // Tambah kolom tanggal
-            $table->timestamp('tanggal_pengajuan')->nullable();
-            $table->timestamp('tanggal_persetujuan')->nullable();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::table('irs', function (Blueprint $table) {
-            $table->enum('status', [
-                'Belum Disetujui',
-                'Disetujui',
-                'Ditolak'
-            ])->default('Belum Disetujui')->change();
-
-            $table->dropColumn('tanggal_pengajuan');
-            $table->dropColumn('tanggal_persetujuan');
-        });
+        //
     }
 };
